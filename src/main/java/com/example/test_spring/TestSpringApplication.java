86ea1815fb1ba01fdfb5ac7991e2b9c681255ba4
VimukthiWaththegama <vwaththegama@gmail.com>
2025-04-04 23:22:00 +0530

    initial commit

---
+++ src/main/java/com/example/test_spring/TestSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringApplication.class, args);
+	}
+
+}
